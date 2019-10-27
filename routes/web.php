a3fe160492a9b9466481fd68cb9df27e6c4fe98a
Rya1811 <arya.wcksn225@gmail.com>
2019-10-27 20:37:44 +0700

    Add: Basic CRUD

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$app->get('/todo', 'TodoController@index');
+$app->get('/todo/{id}', 'TodoController@show');
+$app->post('/todo', 'TodoController@store');
